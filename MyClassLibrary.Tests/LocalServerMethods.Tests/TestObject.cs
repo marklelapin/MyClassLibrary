18e72c881676f5ebe173a8dd8e013205835758f9
Mark Carter <magcarter@hotmail.co.uk>
2023-04-05 18:21:46 +0100

    Started work on tests for Local Server Methods. Setting up appsettings.json file and ConnectionSTring class to access connection strings.

---
+++ MyClassLibrary.Tests/LocalServerMethods.Tests/TestObject.cs
@@ -0,0 +1,33 @@
+﻿using MyClassLibrary.LocalServerMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary.Tests.LocalServerMethods.Tests
+{
+    internal class TestObject : LocalServerIdentity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime FavouriteDate { get; set; }
+
+        public int FavouriteInteger { get; set; }
+
+        public List<string> FavouriteFoods { get; set; }
+
+
+        public TestObject(string firstName, string lastName, DateTime favouriteDate, int favouriteInteger, List<string> favouriteFoods)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            FavouriteDate = favouriteDate;
+            FavouriteInteger = favouriteInteger;
+            FavouriteFoods = favouriteFoods;
+        }
+
+
+    }
+}
