68caf7ea20992d8baa060ed69ff70dee4ca7b581
Mark Carter <magcarter@hotmail.co.uk>
2023-04-12 17:23:26 +0100

    Add Guid Extension to generate list of guids

---
+++ MyClassLibrary/Extensions/GuidExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary.Extensions
+{
+    public static class GuidExtensions
+    {
+        public static List<Guid> GenerateList(this Guid guid,int quantity)
+        {
+            List<Guid> output = new List<Guid>();
+            for (int i = 0; i < quantity; i++)
+            {
+                output.Add(Guid.NewGuid());
+            }
+
+            return output;
+            
+        }
+    }
+}
