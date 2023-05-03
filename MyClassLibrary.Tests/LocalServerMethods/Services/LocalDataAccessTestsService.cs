be9c1c95b4f2118a8b8af9c5a026d867949bca4a
Mark Carter <magcarter@hotmail.co.uk>
2023-05-03 15:21:18 +0100

    Changes to LocalDataAccessTestsService and LocalDataAccessTests to get setup correct for multiple use.

--- MyClassLibrary.Tests/LocalServerMethods/Services/LocalDataAccessTestsService.cs
+++ MyClassLibrary.Tests/LocalServerMethods/Services/LocalDataAccessTestsService.cs
@@ -14,62 +14,59 @@ namespace MyClassLibrary.Tests.LocalServerMethods.Services
 {
     public class LocalDataAccessTestsService<T> : ILocalDataAccessTests<T> where T : LocalServerIdentityUpdate
     {
-        
-        private static IServiceConfiguration _serviceConfiguration;
 
-        private static IServiceConfiguration ServiceConfiguration
+        private ITestContent<T> _testContent;
+
+        private IServiceConfiguration _serviceConfiguration;
+        private readonly ILocalDataAccess _localDataAccess;
+
+        public LocalDataAccessTestsService(IServiceConfiguration serviceConfiguration)
         {
-            get { return _serviceConfiguration; }
-            set { _serviceConfiguration = value; }
+            _serviceConfiguration = serviceConfiguration;
+            _localDataAccess = _serviceConfiguration.LocalDataAccess();
+            _testContent = _serviceConfiguration.TestContent<T>();
         }
-        public LocalDataAccessTestsService(IServiceConfiguration? serviceConfiguration = null)
-        {
-            _serviceConfiguration = serviceConfiguration ?? throw new ArgumentNullException("No Services Configuration passed through for Testing.");
-        }
-  
 
 
-        private static ILocalDataAccess localDataAccess = ServiceConfiguration.LocalDataAccess();
-
-        private static ITestContent<T> testContent = ServiceConfiguration.TestContent<T>();
-
-        public static readonly object[][] SaveTestUpdates = { new object[] { testContent.Generate(1, "Default")[0] } };
-        [Theory, MemberData(nameof(SaveTestUpdates))]
+        public object[][] SaveTestData() { return new object[][] { new object[] { _testContent.Generate(1, "Default")[0] } }; }
+        //[Theory, MemberData(nameof(SaveTestData))]
         public void SaveTest(List<T> updates)
         {
-            localDataAccess.SaveToLocal(updates);
+            _localDataAccess.SaveToLocal(updates);
             Assert.True(true);
         }
 
 
 
-        private static readonly List<List<T>> SaveAndGetTestContents = testContent.Generate(3, "Default");
-        public static readonly object[][] SaveAndGetTestData =
+        private List<List<T>> SaveAndGetTestContents() { return _testContent.Generate(3, "Default"); }
+        public object[][] SaveAndGetTestData()
         {
-            new object[] {
-                            SaveAndGetTestContents[0]
-                            ,testContent.ListIds(SaveAndGetTestContents[0])
-                            ,SaveAndGetTestContents[0]
-                           },
-            new object[]
-                        {
-                            SaveAndGetTestContents[1]
-                            ,testContent.ListIds(SaveAndGetTestContents[1])
-                            ,SaveAndGetTestContents[1]
-                           },
-            new object[]
-                        {
-                            SaveAndGetTestContents[2]
-                            ,testContent.ListIds(SaveAndGetTestContents[2])[2]
-                            ,SaveAndGetTestContents[2].Where(x => x.Id == testContent.ListIds(SaveAndGetTestContents[2])[2]).ToList()
-                           }
-        };
-        [Theory, MemberData(nameof(SaveAndGetTestData))]
+            return new object[][] {
+                            new object[] {
+                                            SaveAndGetTestContents()[0]
+                                            ,_testContent.ListIds(SaveAndGetTestContents()[0])
+                                            ,SaveAndGetTestContents()[0]
+                                           },
+                            new object[]
+                                        {
+                                            SaveAndGetTestContents()[1]
+                                            ,_testContent.ListIds(SaveAndGetTestContents()[1])
+                                            ,SaveAndGetTestContents()[1]
+                                           },
+                            new object[]
+                                        {
+                                            SaveAndGetTestContents()[2]
+                                            ,_testContent.ListIds(SaveAndGetTestContents()[2])[2]
+                                            ,SaveAndGetTestContents()[2].Where(x => x.Id == _testContent.ListIds(SaveAndGetTestContents()[2])[2]).ToList()
+                                           }
+            };
+        }
+        // [Theory, MemberData(nameof(SaveAndGetTestData))]
         public void SaveAndGetTest(List<T> testUpdates, List<Guid> idsToGet, List<T> expected)
         {
-            localDataAccess.SaveToLocal(testUpdates);
+            _localDataAccess.SaveToLocal(testUpdates);
 
-            List<T> actual = localDataAccess.GetFromLocal<T>(idsToGet);
+            List<T> actual = _localDataAccess.GetFromLocal<T>(idsToGet);
 
             actual.Sort((x, y) => x.Id.CompareTo(y.Id));
             expected.Sort((x, y) => x.Id.CompareTo(y.Id));
@@ -79,17 +76,17 @@ namespace MyClassLibrary.Tests.LocalServerMethods.Services
 
 
 
-        public static readonly object[][] GetChangesTestContent = { new object[] { testContent.Generate(1, "Default")[0] } };
-        [Theory, MemberData(nameof(GetChangesTestContent))]
+        public object[][] GetChangesTestData() { return new object[][] { new object[] { _testContent.Generate(1, "Default")[0] } }; }
+        //[Theory, MemberData(nameof(GetChangesTestData))]
         public void GetChangesTest(List<T> updates) //also tests functionality of passing null into GetFromLocal
         {
 
-            localDataAccess.SaveToLocal(updates);
-            List<T> allUpdates = localDataAccess.GetFromLocal<T>(null);
+            _localDataAccess.SaveToLocal(updates);
+            List<T> allUpdates = _localDataAccess.GetFromLocal<T>(null);
 
             List<T> expected = allUpdates.Where(x => x.UpdatedOnServer == null).ToList();
 
-            List<T> actual = localDataAccess.GetChangesFromLocal<T>();
+            List<T> actual = _localDataAccess.GetChangesFromLocal<T>();
 
             actual.Sort((x, y) => x.Id.CompareTo(y.Id));
             expected.Sort((x, y) => x.Id.CompareTo(y.Id));
@@ -98,73 +95,70 @@ namespace MyClassLibrary.Tests.LocalServerMethods.Services
         }
 
 
-
-        [Fact]
-        public void SaveAndGetLocalLastSyncDate()
+        public object[][] SaveAndGetLocalLastSyncDateTestData() { return new object[][] { new object[] { DateTime.Now } }; }
+        // [Theory, MemberData(nameof(SaveandGetLocalLastSyncDateTestData))]
+        public void SaveAndGetLocalLastSyncDateTest(DateTime expected)
         {
-            DateTime expected = DateTime.Now;
-            localDataAccess.SaveLocalLastSyncDate<T>(expected);
+            _localDataAccess.SaveLocalLastSyncDate<T>(expected);
 
-            DateTime actual = localDataAccess.GetLocalLastSyncDate<T>();
+            DateTime actual = _localDataAccess.GetLocalLastSyncDate<T>();
 
             Assert.Equal(expected, actual);
         }
 
 
 
-        public static readonly object[][] SaveUpdatedOnServerTestContent = { new object[] { testContent.Generate(1, "Default")[0] } };
-        [Theory, MemberData(nameof(SaveUpdatedOnServerTestContent))]
+        public object[][] SaveUpdatedOnServerTestData() { return new object[][] { new object[] { _testContent.Generate(1, "Default")[0] } }; }
+        //[Theory, MemberData(nameof(SaveUpdatedOnServerTestData))]
         public void SaveUpdatedOnServerTest(List<T> updates)
         {
             DateTime updatedOnServer = DateTime.Now;
 
-            updates = testContent.Generate(1, "default")[0];
+            updates = _testContent.Generate(1, "default")[0];
 
-            localDataAccess.SaveToLocal(updates);
+            _localDataAccess.SaveToLocal(updates);
 
-            localDataAccess.SaveUpdatedOnServerToLocal(updates, updatedOnServer);
+            _localDataAccess.SaveUpdatedOnServerToLocal(updates, updatedOnServer);
 
-            List<T> actualUpdated = localDataAccess.GetFromLocal<T>(testContent.ListIds(updates));
+            List<T> actualUpdated = _localDataAccess.GetFromLocal<T>(_testContent.ListIds(updates));
 
-            List<T> actualNotUpdated = localDataAccess.GetFromLocal<T>().Where(x => !actualUpdated.Any(y => y.Id == x.Id)).ToList();
+            List<T> actualNotUpdated = _localDataAccess.GetFromLocal<T>().Where(x => !actualUpdated.Any(y => y.Id == x.Id)).ToList();
 
             Assert.True(actualNotUpdated.Where(x => x.UpdatedOnServer == updatedOnServer).Count() == 0, "No Other Objects Have UpdatedOnServerDate");
             Assert.True(actualUpdated.Where(x => x.UpdatedOnServer == updatedOnServer).Count() == updates.Count());
         }
 
 
-        public static readonly List<List<T>> saveConflictIDTestContents = testContent.Generate(2,"Default");
 
-        public static List<Conflict> conflicts { get
-                 {
-                List<Conflict> output = new List<Conflict>();
-                List<T> conflictedObjects = new List<T>();
-                conflictedObjects.Add(saveConflictIDTestContents[0][1]);
-                conflictedObjects.Add(saveConflictIDTestContents[0][5]);
-                
-                conflicts.Add(new Conflict(conflictedObjects[0].Id, conflictedObjects[0].Created, Guid.NewGuid()));
-                conflicts.Add(new Conflict(conflictedObjects[1].Id, conflictedObjects[1].Created, Guid.NewGuid()));
-
-                return output;
-                }
-         } 
-
-
-        public static readonly object[][] saveConflictIDTestData =
+        private List<List<T>> SaveConflictIdTestContents() { return _testContent.Generate(2, "Default"); }
+        private List<Conflict> Conflicts()
         {
-            new object[]{saveConflictIDTestContents[0],conflicts,conflicts},
-            new object[]{saveConflictIDTestContents[1],new List<Conflict>(),new List<Conflict>()}
-        };
+            List<Conflict> output = new List<Conflict>();
+            List<T> conflictedObjects = new List<T>();
+            conflictedObjects.Add(SaveConflictIdTestContents()[0][1]);
+            conflictedObjects.Add(SaveConflictIdTestContents()[0][5]);
 
-        [Theory, MemberData(nameof(saveConflictIDTestData))]
+            output.Add(new Conflict(conflictedObjects[0].Id, conflictedObjects[0].Created, Guid.NewGuid()));
+            output.Add(new Conflict(conflictedObjects[1].Id, conflictedObjects[1].Created, Guid.NewGuid()));
+
+            return output;
+        }
+        public object[][] SaveConflictIdTestData()
+        {
+            return new object[][] {
+                    new object[]{SaveConflictIdTestContents()[0],Conflicts(),Conflicts()},
+                    new object[]{SaveConflictIdTestContents()[1],new List<Conflict>(),new List<Conflict>()}
+            };
+        }
+        //[Theory, MemberData(nameof(saveConflictIdTestData))]
         public void SaveConflictIdTest(List<T> testUpdates, List<Conflict> conflicts, List<Conflict> expected)
         {
-            localDataAccess.SaveToLocal(testUpdates);
-            localDataAccess.SaveConflictIdsToLocal<T>(conflicts);
+            _localDataAccess.SaveToLocal(testUpdates);
+            _localDataAccess.SaveConflictIdsToLocal<T>(conflicts);
 
             List<Conflict> actual = new List<Conflict>();
 
-            actual =localDataAccess.GetFromLocal<T>(testContent.ListIds(testUpdates))
+            actual = _localDataAccess.GetFromLocal<T>(_testContent.ListIds(testUpdates))
                                                     .Where(x => x.ConflictId != null)
                                                     .Select(x => new Conflict(x.Id, x.Created, x.ConflictId))
                                                     .ToList();
@@ -177,18 +171,18 @@ namespace MyClassLibrary.Tests.LocalServerMethods.Services
 
 
 
-        public static readonly object[][] DeleteTestContent = { new object[] { testContent.Generate(1, "Default")[0] } };
-        [Theory, MemberData(nameof(DeleteTestContent))]
-        public void DeleteTest(List<T> testUpdates)
+        public object[][] DeleteTestData() { return new object[][] { new object[] { _testContent.Generate(1, "Default")[0] } };}
+        //[Theory, MemberData(nameof(DeleteTestContent))]
+        public void DeleteTestData(List<T> testUpdatesToDelete)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(testUpdatesToDelete.ToString());
         }
 
 
-        //async static private void InsertDelay(int milliSeconds)
-        //{
-        //    await Task.Delay(milliSeconds);
-        //}
+        async private void InsertDelay(int milliSeconds)
+        {
+            await Task.Delay(milliSeconds);
+        }
 
     }
 }
---
+++ MyClassLibrary.Tests/LocalServerMethods/Tests/LocalDataAccessTests.cs
@@ -0,0 +1,29 @@
+﻿using MyClassLibrary.Tests.LocalServerMethods.Interfaces;
+using MyClassLibrary.Tests.LocalServerMethods.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary.Tests.LocalServerMethods.Tests
+{
+    public class LocalDataAccessTests
+    {
+        private static IServiceConfiguration _serviceConfiguration = new TestServiceConfiguration();
+
+        private static ILocalDataAccessTests<TestUpdate> _localDataAccessTests = new LocalDataAccessTestsService<TestUpdate>(_serviceConfiguration);
+
+        //private static ITestContent<TestUpdate> _testContent = new TestContentService<TestUpdate>();
+
+        public static object[][] SaveTestData = _localDataAccessTests.SaveTestData();
+        [Theory, MemberData(nameof(SaveTestData))]
+        public void TestUpdateSaveTest(List<TestUpdate> testUpdates)
+        {
+            _localDataAccessTests.SaveTest(testUpdates);
+        }
+            
+            
+
+    }
+}
