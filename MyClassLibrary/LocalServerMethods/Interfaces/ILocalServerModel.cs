94515e6f2d7184ab05c01dd0b1e4a35655890ff8
Mark Carter <magcarter@hotmail.co.uk>
2023-06-06 17:30:55 +0100

    XML Comments added to LocalServerModela and Update

--- MyClassLibrary/LocalServerMethods/Interfaces/ILocalServerModel.cs
+++ MyClassLibrary/LocalServerMethods/Interfaces/ILocalServerModel.cs
@@ -8,9 +8,21 @@ namespace MyClassLibrary.LocalServerMethods.Interfaces
     /// <typeparam name="T"></typeparam>
     public interface ILocalServerModel<T> where T : ILocalServerModelUpdate
     {
+        /// <summary>
+        /// A list of Updates that conflict with the Latest Update. i.e. that were created on different local machines without the knowledge of each one.
+        /// </summary>
         List<T>? Conflicts { get; set; }
+        /// <summary>
+        /// All of the updates relating to the model.
+        /// </summary>
         List<T>? History { get; set; }
+        /// <summary>
+        /// The unique Id identifying the model.
+        /// </summary>
         Guid Id { get; }
+        /// <summary>
+        /// The latest update relating to the model.
+        /// </summary>
         T? Latest { get; set; }
     }
 }
--- MyClassLibrary/LocalServerMethods/Interfaces/ILocalServerModelUpdate.cs
+++ MyClassLibrary/LocalServerMethods/Interfaces/ILocalServerModelUpdate.cs
@@ -5,11 +5,29 @@
     /// </summary>
     public interface ILocalServerModelUpdate
     {
+        /// <summary>
+        /// Indicates if other updates exist that were created locally and seperately on different local machines before syncing could take place.
+        /// </summary>
         bool IsConflicted { get; set; }
+        /// <summary>
+        /// The date and time the update was created on the local machine.
+        /// </summary>
         DateTime Created { get; set; }
+        /// <summary>
+        /// The user who created the update.
+        /// </summary>
         string CreatedBy { get; set; }
+        /// <summary>
+        /// The id of the model for which this update relates.
+        /// </summary>
         Guid Id { get; set; }
+        /// <summary>
+        /// Indicates wether or not the model should be generally viewed. (Soft Delete)
+        /// </summary>
         bool IsActive { get; set; }
+        /// <summary>
+        /// The date and time that the update was saved to the central server storage.
+        /// </summary>
         DateTime? UpdatedOnServer { get; set; }
     }
 }
