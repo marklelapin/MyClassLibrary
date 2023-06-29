e7cfc2cec5a0d794e27cca4fb15367e1e23b9c0a
Mark Carter <magcarter@hotmail.co.uk>
2023-06-29 14:08:43 +0100

    minor changes

--- MyApiMonitorClassLibrary/Interfaces/IApiTestCollectionFactory.cs
+++ MyApiMonitorClassLibrary/Interfaces/IApiTestCollectionFactory.cs
@@ -5,8 +5,13 @@ namespace MyApiMonitorClassLibrary.Interfaces
     public interface IApiTestCollectionFactory
     {
         /// <summary>
-        /// Generates the test collections to be used by Api Test Monitor
+        /// Generates the test collections to be executed.
         /// </summary>
         public List<ApiTestCollection> GenerateTestCollections();
+
+        /// <summary>
+        /// Runs and save the result of each test collection.
+        /// </summary>
+        public Task ExecuteTestCollections(List<ApiTestCollection> testCollections);
     }
 }
