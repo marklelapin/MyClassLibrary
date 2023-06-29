45f94179ac752a0fd1a4fb8b9a565d27884d678a
Mark Carter <magcarter@hotmail.co.uk>
2023-06-29 14:10:34 +0100

    Beginning of adding axes to ChartJs Models

--- MyClassLibrary/ChartJs/Axes.cs
+++ MyClassLibrary/ChartJs/Axes.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyClassLibrary.ChartJs
+﻿namespace MyClassLibrary.ChartJs
 {
-    internal class Axes
+    public class Axes
     {
+
     }
 }
