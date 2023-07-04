5214d80cf6282ac18e9fa1fd5a3abb1eaa6f72bf
Mark Carter <magcarter@hotmail.co.uk>
2023-07-04 14:17:03 +0100

    CRON corrections to ApiMonitorFunctions plus availabilty tests corrected.

---
+++ MyApiMonitorFunctions/RunTestCollections.cs
@@ -0,0 +1,40 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+
+namespace MyApiMonitorFunctions
+{
+    public class RunTestCollections
+    {
+        [FunctionName("RunTestCollections")]
+        public void RunCollections([TimerTrigger("0 15 */4 * * *")] TimerInfo myRunCollectionsTimer, ILogger log)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://myapimonitorserviceapi.azurewebsites.net/");
+
+            var getTask = client.GetAsync("runtestcollections");
+            getTask.Wait();
+            HttpResponseMessage response = getTask.Result;
+
+            log.LogInformation($"MyApiMonitorServiceApi ran at {DateTime.Now} with response: {response.StatusCode}");
+
+
+            // log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+
+        [FunctionName("RunAvailabilityTests")]
+        public void RunAvailabilityTests([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://myapimonitorserviceapi.azurewebsites.net/");
+            var getTask = client.GetAsync("runavailabilitytests");
+            getTask.Wait();
+            HttpResponseMessage response = getTask.Result;
+
+            //log.LogInformation($"AvailabilityTest ran at {DateTime.Now} with response: {response.StatusCode}");
+        }
+
+    }
+}
