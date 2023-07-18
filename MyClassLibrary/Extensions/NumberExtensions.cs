346db9910cf226fc18d33f5ce4b49f20c49fdedc
Mark Carter <magcarter@hotmail.co.uk>
2023-07-18 14:56:17 +0100

    Added Int64 and double extension to convert javascript timestamp to Date Time.

---
+++ MyClassLibrary/Extensions/NumberExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MyClassLibrary.Extensions
+{
+    public static class NumberExtensions
+    {
+        public static DateTime JavascriptTicksToDate(this Int64 javascriptTicks)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(javascriptTicks);
+        }
+
+
+        public static DateTime JavascriptTicksToDate(this double javascriptTicks)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(javascriptTicks);
+        }
+
+    }
+}
