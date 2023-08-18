7cf22d2cab324b2cba4aa5c25c3b04147b0c6e41
Mark Carter <magcarter@hotmail.co.uk>
2023-08-18 11:20:50 +0100

    minor change to results page

--- MyApiMonitor/Pages/Results.cshtml.cs
+++ MyApiMonitor/Pages/Results.cshtml.cs
@@ -7,51 +7,51 @@ namespace MyApiMonitor.Pages.Results;
 
 public class ResultsModel : PageModel
 {
-    private readonly IApiTestDataAccess _dataAccess;
+	private readonly IApiTestDataAccess _dataAccess;
 
-    public ResultsModel(IApiTestDataAccess dataAccess)
-    {
-        _dataAccess = dataAccess;
-    }
+	public ResultsModel(IApiTestDataAccess dataAccess)
+	{
+		_dataAccess = dataAccess;
+	}
 
 
-    [BindProperty]
-    public List<ApiTestData> TestResults { get; set; } = new List<ApiTestData>();
+	[BindProperty]
+	public List<ApiTestData> TestResults { get; set; } = new List<ApiTestData>();
 
-    [BindProperty]
-    public string DateFrom { get; set; }
+	[BindProperty]
+	public string DateFrom { get; set; }
 
-    [BindProperty]
-    public string DateTo { get; set; }
+	[BindProperty]
+	public string DateTo { get; set; }
 
-    [BindProperty]
-    public string CollectionTitle { get; set; }
+	[BindProperty]
+	public string CollectionTitle { get; set; }
 
-    [BindProperty]
-    public string TestDateTime { get; set; }
+	[BindProperty]
+	public string TestDateTime { get; set; }
 
-    public async Task OnGet(Guid testOrCollectionId, DateTime? dateFrom = null, DateTime? dateTo = null, int skip = 0, int limit = 1000)
-    {
-        dateFrom = dateFrom ?? DateTime.MinValue; //DateTime dtFrom = (dateFrom == null) ? DateTime.MinValue : DateTime.Parse(dateFrom);
-        dateTo = dateTo ?? DateTime.MaxValue; //DateTime dtTo = (dateTo == null) ? DateTime.MaxValue : DateTime.Parse(dateTo);
+	public async Task OnGet(Guid testOrCollectionId, DateTime? dateFrom = null, DateTime? dateTo = null, int skip = 0, int limit = 1000)
+	{
+		dateFrom = dateFrom ?? DateTime.MinValue; //DateTime dtFrom = (dateFrom == null) ? DateTime.MinValue : DateTime.Parse(dateFrom);
+		dateTo = dateTo ?? DateTime.MaxValue; //DateTime dtTo = (dateTo == null) ? DateTime.MaxValue : DateTime.Parse(dateTo);
 
-        (TestResults, int totalRecords) = await _dataAccess.GetAllByCollectionId(testOrCollectionId, dateFrom, dateTo, skip, limit);
+		(TestResults, int totalRecords) = await _dataAccess.GetAllByCollectionId(testOrCollectionId, dateFrom, dateTo, skip, limit);
 
-        if (totalRecords == 0)
-        { //if it can't find any using the guid passed in as collection Id then try using it as test Id.
+		if (totalRecords == 0)
+		{ //if it can't find any using the guid passed in as collection Id then try using it as test Id.
 
-            (TestResults, totalRecords) = await _dataAccess.GetAllByTestId(testOrCollectionId, dateFrom, dateTo, skip, limit);
+			(TestResults, totalRecords) = await _dataAccess.GetAllByTestId(testOrCollectionId, dateFrom, dateTo, skip, limit);
 
-        }
+		}
 
-        TestResults = TestResults.OrderByDescending(x => x.TestDateTime).ToList();
+		TestResults = TestResults.OrderByDescending(x => x.TestDateTime).ToList();
 
-        DateFrom = dateFrom.ToString() ?? string.Empty;
-        DateTo = dateTo.ToString() ?? string.Empty;
-        CollectionTitle = (TestResults.Count == 0) ? "" : TestResults.First().CollectionTitle;
-        TestDateTime = (TestResults.Count == 0) ? "" : TestResults.First().TestDateTime.ToString();
+		DateFrom = dateFrom.ToString() ?? string.Empty;
+		DateTo = dateTo.ToString() ?? string.Empty;
+		CollectionTitle = (TestResults.Count == 0) ? "" : TestResults.First().CollectionTitle;
+		TestDateTime = (TestResults.Count == 0) ? "" : TestResults.First().TestDateTime.ToString();
 
-    }
+	}
 }
 
 
