8f27c38f0fdb67d922fc843df7aefa9e974d4593
Mark Carter <magcarter@hotmail.co.uk>
2023-10-28 12:28:08 +0100

    new constructor added.

--- MyClassLibrary/LocalServerMethods/Models/LocalServerModel.cs
+++ MyClassLibrary/LocalServerMethods/Models/LocalServerModel.cs
@@ -1,41 +1,46 @@
-﻿using Microsoft.Identity.Client;
-using MyClassLibrary.LocalServerMethods.Interfaces;
+﻿using MyClassLibrary.LocalServerMethods.Interfaces;
 
 
 namespace MyClassLibrary.LocalServerMethods.Models
 {
-    
-    public class LocalServerModel<T> : ILocalServerModel<T> where T : ILocalServerModelUpdate, new()
-    {
-        /// <summary>
-        /// The Identity of the LocalServerIdentity
-        /// </summary>
-        public Guid Id { get; set; } = Guid.Empty;
 
-        /// <summary>
-        /// The Latest update to the LocalServerIdentity.
-        /// </summary>
-        /// <return>
-        /// Returns null if the latest update is deactivated.
-        /// </return>
-        public T? Latest { get; set; }
-        /// <summary>
-        /// All the updates with the same conflictId as the latest update for the LocalServerIdentity
-        /// </summary>
-        public List<T>? Conflicts { get; set; }
-        /// <summary>
-        /// All updates relating to the LocalServerIdentity
-        /// </summary>
-        public List<T>? History { get; set; }
+	public class LocalServerModel<T> : ILocalServerModel<T> where T : ILocalServerModelUpdate, new()
+	{
+		/// <summary>
+		/// The Identity of the LocalServerIdentity
+		/// </summary>
+		public Guid Id { get; set; } = Guid.Empty;
 
-
-        public LocalServerModel()
-        {
-        
-        }
+		/// <summary>
+		/// The Latest update to the LocalServerIdentity.
+		/// </summary>
+		/// <return>
+		/// Returns null if the latest update is deactivated.
+		/// </return>
+		public T? Latest { get; set; }
+		/// <summary>
+		/// All the updates with the same conflictId as the latest update for the LocalServerIdentity
+		/// </summary>
+		public List<T>? Conflicts { get; set; }
+		/// <summary>
+		/// All updates relating to the LocalServerIdentity
+		/// </summary>
+		public List<T>? History { get; set; }
 
 
-    }
+		public LocalServerModel()
+		{
+
+		}
+
+		public LocalServerModel(T update)
+		{
+			Id = update.Id;
+			Latest = update;
+			Conflicts = new List<T>();
+			History = new List<T>() { update };
+		}
+	}
 
 }
 
