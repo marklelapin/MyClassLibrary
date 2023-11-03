f40d44cb544d3eba30361ca761bd857413253a2f
Mark Carter <magcarter@hotmail.co.uk>
2023-11-03 13:48:32 +0000

    addition of CamelCaseNamePolicy

---
+++ MyClassLibrary/Configuration/CamelCaseNamingPolicy.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text.Json;
+
+namespace MyClassLibrary.Configuration
+{
+	public class CamelCaseNamingPolicy : JsonNamingPolicy
+	{
+		public override string ConvertName(string name)
+		{
+			// Convert the name to camelCase
+			if (string.IsNullOrEmpty(name))
+				return name;
+			if (name.Length == 1)
+				return name.ToLowerInvariant();
+			return char.ToLowerInvariant(name[0]) + name.Substring(1);
+		}
+	}
+}
